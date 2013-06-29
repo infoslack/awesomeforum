36b7eb4702dea1ecff6186924d13b96b648d77b5
Daniel Romero <infoslack@gmail.com>
2013-06-29 04:16:15 -0300

    added specs for reply to question

--- spec/features/reply_to_question_spec.rb
+++ spec/features/reply_to_question_spec.rb
@@ -15,8 +15,9 @@ feature "Reply to question" do
     expect(page).to have_content(notice("answers.create"))
     expect(page).to have_content("This is the correct answer")
   end
+
   scenario "when sending invalid data" do
-     question = questions(:ruby)
+    question = questions(:ruby)
 
     login_as users(:john)
     visit root_path
@@ -27,5 +28,19 @@ feature "Reply to question" do
     expect(current_path).to eql(question_path(question))
     expect(page).to have_content(t("activerecord.errors.messages.blank"))
   end
-  scenario "when unlogged"
+
+  scenario "when unlogged" do
+    question = questions(:ruby)
+    visit root_path
+    click_link question.title
+
+    translation = t("question.must_be_logged_to_answer_html",
+      url: login_path(return_to: question_path(question))
+    ).strip
+
+    html = Nokogiri::HTML(page.body)
+    actual = html.css(".disclaimer").inner_html.strip
+
+    expect(actual).to eql(translation)
+  end
 end
